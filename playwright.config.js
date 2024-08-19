5d8ddac35c931d6ab116a899bb011145a07d7bfe
AmitpGit23 <90781369+AmitpGit23@users.noreply.github.com>
2024-08-19 10:45:22 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -39,15 +39,15 @@ module.exports = defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
+    // {
+    //   name: 'chromium',
+    //   use: { ...devices['Desktop Chrome'] },
+    // },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
     {
       name: 'webkit',
